40b6f055bb5ed26f4c262cbc9b6429e69786ceb6
aleksey <maylo.kondratev@yandex.ru>
2018-10-09 22:21:08 +0300

    Вынес две секции в отдельные файлы, новая таблица стилей

---
+++ include/help.php
@@ -0,0 +1,31 @@
+<section class="section section-features">
+	<div class="container">
+		<div class="section__title">
+			<h2 class="title title-h2 center">Поможем выбрать школу мечты</h2>
+		</div>
+
+		<div class="features row">
+			<div class="features__item col-md-4">
+				<a class="features__link">
+					<div class="features__pic"><img src="images/school.svg" /></div>
+					<span>Запись в школу <br>и решение любых вопросов</span>
+				</a>
+			</div>
+
+			<div class="features__item col-md-4">
+				<a class="features__link">
+					<div class="features__pic"><img src="images/visa.svg" /></div>
+					<span>Помощь в оформление <br>визы</span>
+				</a>
+			</div>
+
+			<div class="features__item col-md-4">
+				<a class="features__link">
+					<div class="features__pic"><img src="images/operator.svg" /></div>
+					<span>Консультация по выбору <br>школу</span>
+				</a>
+			</div>
+		</div>
+
+	</div>
+</section>
